0d928956e8230d8110de4a327418c6c79c4d4c97
Olga Sitnikova <72819725+Olyathecute@users.noreply.github.com>
2023-12-08 21:55:10 +0700

    environment setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,13 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      <h1>Netflix clone</h1>
+      <h1 className='text-green-500'>Netflix clone</h1>
     </>
   )
 }
+
+// sm: 640px
+// md: 768px
+// lg: 1024px
+// xl: 1280px;
+// 2xl: 1536px
