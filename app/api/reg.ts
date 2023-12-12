588d6c9bb583f8da764932ac97f30f2029b0e38b
Olga Sitnikova <72819725+Olyathecute@users.noreply.github.com>
2023-12-12 23:51:48 +0700

    auth an mongo setup

---
+++ app/api/reg.ts
@@ -0,0 +1,35 @@
+'use server'
+import bcrypt from 'bcrypt'
+import { NextApiRequest, NextApiResponse } from 'next'
+import prismadb from '@/lib/prismadb'
+
+export async function registerUser(email: string, name: string, password: string) {
+  try {
+    const existingUser = await prismadb.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (existingUser) {
+      throw new Error('email take')
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    const user = await prismadb.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+        image: ' ',
+        emailVerified: new Date(),
+      },
+    })
+
+    return user
+  } catch (error) {
+    console.log('Here', error)
+    throw new Error('SOLLY MISTA')
+  }
+}
--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,20 +2,48 @@
 import { useCallback, useState } from 'react'
 import Input from '@/components/Input'
 import { authText, authPageType } from '../data'
+import { registerUser } from '../api/reg'
+import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 type Props = {}
 
 function Auth({}: Props) {
+  const router = useRouter()
+
   const [form, setForm] = useState({ email: '', name: '', password: '' })
   console.log(form)
 
   const [variant, setVariant] = useState<authPageType>('login')
+  const texts = authText[variant]
 
   const toggleVariant = useCallback(() => {
     setVariant((prev) => (prev === 'login' ? 'register' : 'login'))
   }, [])
 
-  const texts = authText[variant]
+  const login = useCallback(async () => {
+    try {
+      await signIn('credentials', {
+        email: form.email,
+        password: form.password,
+        redirect: false,
+        callbackUrl: '/',
+      })
+
+      router.push('/')
+    } catch (error) {
+      console.log('Error in login', error)
+    }
+  }, [form.email, form.password, router])
+
+  const register = useCallback(async () => {
+    try {
+      registerUser(form.email, form.name, form.password)
+      login()
+    } catch (error) {
+      console.log('Error in register', error)
+    }
+  }, [form])
 
   return (
     <div className='relative h-full w-full bg-hero bg-no-repeat bg-center bg-fixed bg-cover'>
@@ -24,7 +52,7 @@ function Auth({}: Props) {
           <img src='/images/logo.png' alt='logo' className='h-12' />
         </nav>
         <div className='flex justify-center'>
-          <div className='bg-black bg-opacity-70 p-12 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
+          <div className='bg-black bg-opacity-70 p-12 self-center mt-2 rounded-md w-full sm:w-4/5 lg:w-2/5 lg:max-w-md '>
             <h2 className='text-white text-4xl mb-8 font-semibold'>{texts.title}</h2>
             <div className='flex flex-col gap-4'>
               {variant === 'register' && (
@@ -50,7 +78,10 @@ function Auth({}: Props) {
                 label='Password'
               />
             </div>
-            <button className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
+            <button
+              onClick={variant === 'login' ? login : register}
+              className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'
+            >
               {texts.button}
             </button>
             <p className='text-neutral-500 mt-12'>
